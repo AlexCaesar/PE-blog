52f7a5d7478559d32eb6b7d4ce3422ed0b7dfbf9
Er.Z <er@zhangabc.com>
2011-08-27 18:17:50 +0800

    添加一路径处理

---
+++ path.c
@@ -0,0 +1,16 @@
+char * get_base_path( char* str){
+	char *token;
+	char blog_path[100] = "/";
+
+	token = strtok(str, "/");
+	strcat(blog_path , token);
+	while ( (token = strtok(NULL, "/")) != NULL){
+		if( strstr(token ,".php") == NULL){
+			strcat(blog_path , "/");
+			strcat(blog_path , token);
+		}   
+	}   
+	strcat(blog_path , "/");
+	return estrndup(blog_path, strlen(blog_path));
+}
+
